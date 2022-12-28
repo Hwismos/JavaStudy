c4f05b38f70d9a0bc474e21fa8326d4f39e10037
Hwiriric <smk6270@gmail.com>
2022-12-28 17:07:41 +0900

    java.io 패키지 실습

--- part2/Main.java
+++ part2/Main.java
@@ -1,17 +1,35 @@
 package part2;
 
+//import java.lang.*;
+//import java.util.*;
+//import java.awt.*;
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		RemoteControlloer rc1 = new Television("석휘");
-		RemoteControlloer rc2 = new Television("유림");
-		rc1.downChannel();
-		rc2.upVolume();
+		try {
+			OutputStream output = new FileOutputStream("C:\\Users\\최석휘\\Desktop\\output.txt");
+			String str = "자바로 파일 입출력을 해보자!!";
+			byte[] by = str.getBytes();
+			output.write(by);		
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
 		
-		System.out.println(rc1.toString());
-		System.out.println(rc2.toString());
-		
-		RemoteControlloer rcOfRadio = new Radio();
-		rcOfRadio.upVolume();
+		try {
+			File file = new File("C:\\Users\\최석휘\\Desktop\\output.txt");
+			FileReader fr = new FileReader(file);
+			int cur = 0;
+			while ((cur = fr.read()) != -1) {
+				System.out.print((char)cur);
+			}
+			System.out.println();
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.getStackTrace();
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
 	}
 }
