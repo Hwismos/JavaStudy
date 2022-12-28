48363b50cf45de78003867b115af27612e269e8f
Hwiriric <smk6270@gmail.com>
2022-12-28 15:23:52 +0900

    부모 클래스 생성

--- part2/Animal.java
+++ part2/Animal.java
@@ -1,5 +1,33 @@
 package part2;
 
 public class Animal {
+	private String name;
+	private int age;
+	
+	// getter, setter 메소드
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 
+	// 생성자 메소드
+	public Animal() {
+		super();
+	}
+	
+	public void eat() {
+		System.out.println("먹는다.");
+	}
+	
+	public void move() {
+		System.out.println("이동한다.");
+	}
 }
