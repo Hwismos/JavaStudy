9566f1dde0ff2f8d0665d04b15a42cfe3304d031
Hwiriric <smk6270@gmail.com>
2022-12-28 17:48:55 +0900

    DAO(IntArray) 실습, ArrayList의 forEach 문법 실습

--- part2/Main.java
+++ part2/Main.java
@@ -1,32 +1,40 @@
 package part2;
 
-//import java.lang.*;
-//import java.util.*;
-//import java.awt.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String s1 = new String("Book");
-		String s2 = new String("Book");
-		String s3 = "Book";
-		String s4 = "Book";
+//		ArrayController ac1 = new IntArray();
+//		ArrayController ac2 = new IntArray(5);
+//		
+//		for(int i=0; i < 10; i++) {
+//			ac1.add(i);
+//		}
+//		
+//		for(int i=5; i > 3; i--) {
+//			ac2.add(i);
+//		}
+//		
+//		System.out.println(ac1.size());
+//		System.out.println(ac2.size());
+//		
+//		for(int i=0; i < ac2.size(); i++) {
+//			System.out.print(ac2.get(i));
+//		}
+//		System.out.println();
 		
-		compare(s1, s2);  // s1과 s2는 별개의 객체
-		compare(s3, s4);  // s3과 s4는 동일한 객체
+		String[] fruitsArray = {"apple", "banana", "kiwi", "mango"};
+		ArrayList<String> fruits = new ArrayList<>(Arrays.asList(fruitsArray));
 		
-		s4 = s4.toUpperCase();  // toUpperCase 메소드가 새로운 String 객체를 반환
-		System.out.println(s4);  // s4는 다른 객체가 됨
+		// import java.util.function.Consumer;
+		Consumer<String> lambda = item -> System.out.println("item: " + item);
+//		fruits.forEach(lambda);
 		
-		compare(s3, s4);
-	}
-	
-	public static void compare(String s1, String s2) {
-		if (s1 == s2) {
-			System.out.println("두 객체는 동일한 객체");
-		} else {
-			System.out.println("두 객체는 별개의 객체");
-		}
+		fruits.forEach(item 
+				-> System.out.println("item: " + item));
 	}
 }
